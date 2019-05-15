3687092b2b8ca9f8b3f4940d8bd2c39ee96813d2
lhh911 <562850373@qq.com>
2019-05-15 16:59:12 +0800

    添加自启动广播，测试通过

---
+++ module_main/src/main/java/com/xsjqzt/module_main/receive/AppStartReceiver.java
@@ -0,0 +1,24 @@
+package com.xsjqzt.module_main.receive;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.jbb.library_common.utils.ToastUtil;
+import com.jbb.library_common.utils.log.LogUtil;
+import com.xsjqzt.module_main.ui.SplashActivity;
+
+public class AppStartReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ToastUtil.showCustomToast("11全掌通自动启动了");
+        LogUtil.w("AppStartReceiver =  11全掌通自动启动了");
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            ToastUtil.showCustomToast("22全掌通自动启动了");
+            LogUtil.w("AppStartReceiver =  22全掌通自动启动了");
+            Intent i = new Intent(context, SplashActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+}
